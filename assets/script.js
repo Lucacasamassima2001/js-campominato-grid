ab7eab7fc3ff0bdd0e47d70baef12f3c9803a49a
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-04-21 20:38:48 +0200

    ex done, need to rewatch

--- assets/script.js
+++ assets/script.js
@@ -7,42 +7,93 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 
-// GENERARE AL GRIGLIA
+// GENERARE AL GRIGLIA E  SELEZIONO BOTTONE
 const elegrid = document.querySelector(".grid");
-for(let i = 1; i <= 100; i++){
-    elegrid.innerHTML += `<div class="cell"></div>`;
-}
-
-// SELEZIONO BOTTONE
 const btnplay = document.querySelector(".play")
+const easy = parseInt(100);
+const medium = parseInt(81);
+const hard = parseInt(49);
+
+// BOTTONE CON FUNZIONE
 
 btnplay.addEventListener("click",
-function active(){     
-    elegrid.classList.add("active");
-}
+function creategrid(){
+    let diffoption = document.querySelector("#options");
+    let value = diffoption.options[diffoption.selectedIndex].value;
+    console.log(value);
+    if(value <= 100){
+        elegrid.classList.remove("gridmedium","gridhard")
+        elegrid.classList.add("grideasy");
+    } if(value <= 81){
+        elegrid.classList.remove("grideasy","gridhard")
+        elegrid.classList.add("gridmedium")
+    }if (value <= 49){
+        
+        elegrid.classList.add("gridhard")
+       
+    }
 
+
+    elegrid.innerHTML = ""
+        for(let i = 1; i <= value; i++){
+            elegrid.innerHTML += `<div class="cell"></div>`;
+        } 
+        
+        
+        const listcells = document.querySelectorAll(`.cell`);
+        for(let i = 0; i < listcells.length ; i++){
+            const cell = listcells[i];
+            cell.addEventListener("click",
+            function colour(){
+                this.classList.toggle("clicked");
+                cell.innerHTML = i + 1;
+                console.log("cella numero: " + (i + 1))
+            }
+            )
+        }
+    
+}
 )
 
 
 
+    
+    
+
+
+
+
+// function creategrid(){
+    
+//     elegrid.innerHTML = ""
+    
+//     if(diffoption === easy){
+//         for(let i = 1; i <= 60; i++){
+        
+        
+//             elegrid.innerHTML += `<div class="cell"></div>`;
+//         } 
+        
+//         const listcells = document.querySelectorAll(`.cell`);
+//         for(let i = 0; i < listcells.length ; i++){
+//             const cell = listcells[i];
+//             cell.addEventListener("click",
+//             function colour(){
+//                 this.classList.toggle("clicked");
+//                 cell.innerHTML = i + 1;
+//                 console.log("cella numero: " + (i + 1))
+//             }
+//             )
+//         }
+//     }  
+// }
 
 
 
 
 
-// APPLICARE EVENT LISTENER A OGNI CELL
-const listcells = document.querySelectorAll(`.cell`);
 
-for(let i = 0; i < listcells.length ; i++){
-    const cell = listcells[i];
-    cell.addEventListener("click",
-    function colour(){
-        this.classList.toggle("clicked");
-        cell.innerHTML = i + 1;
-        console.log("cella numero: " + (i + 1))
-    }
-    )
-}
+
 
 
 
